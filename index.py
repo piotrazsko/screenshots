93500c43b79f5db12a9119bf94ce92e749677815
serj <piotrazsko@gmail.com>
2020-02-09 15:47:29 +0300

    added_exe

--- index.py
+++ index.py
@@ -18,19 +18,19 @@ def on_press(key):
 
     print('{0} pressed'.format(key))
     if 	key == Key.esc:
- 		return False
+        return False
     if key == Key.enter:
-   		image = pyautogui.screenshot(region=(0,0, 3300, 1000))
-		image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-		inc()
-		cv2.imwrite( str(index)+ '.png' , image)
+        image = pyautogui.screenshot(region=(0,0, 1400, 1000))
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        inc()
+        cv2.imwrite( str(index)+ '.png' , image)
 		# return 
 	
 
 def on_click(x, y, button, pressed):
-   		print("Mouse clicked")
+        print("Mouse clicked")
 
 with Listener( on_press=on_press,on_click=on_click) as listener:
-   	listener.join()
+    listener.join()
 
     
